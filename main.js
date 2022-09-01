a9f07b095c6fc912496a66c25e61bc767bd10e07
Felipe <felipeangelodias@live.com>
2022-09-01 11:20:37 -0300

    O projeto atualizado, feito o acrescimo do camando (.value) nas variaveis 11:19 01.09.22

--- main.js
+++ main.js
@@ -2,19 +2,22 @@ const form = document.getElementById('form-valor');
 
 const campoA = document.getElementById('numbera');
 const campoB = document.getElementById('numberb');
-const mensagemCerto = `O valor: ${campoB.value} do campo B esta maior que o valor : ${campoA.value} do campo A, Sucesso!`;
-const e = "Errado";
+
 
     form.addEventListener('submit', function (a){
-        a.preventDefault();
-            if (campoA < campoB){
-                alert(mensagemCerto);
-            }  else {
-                alert(e);
-            }
-        
-        })
-   
-   
 
+        const mensagemCerto = `Sucesso! o valor: ${campoB.value}  do campo B esta maior que o valor : ${campoA.value} do campo A.`;
+        const e = `Errado, o valor: ${campoA.value} do campo A esta maior ou igual ao valor: ${campoB.value} do campo B`;
+     
+
+            if (campoA.value < campoB.value || campoA.value === campoB.value){
+                alert(e);           
+            }else {
+                alert(mensagemCerto);
+            }
+        });
+    
 console.log(form);
+a.preventDefault();
+
+
